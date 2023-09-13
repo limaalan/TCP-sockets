140c65a761108ec7ad2fa1c53e9ccfcdb8008441
limaalan <alanlimamarques6@gmail.com>
2023-09-13 18:40:27 -0300

    user auth

--- basic_shell/server.py
+++ basic_shell/server.py
@@ -3,6 +3,9 @@ from _thread import *
 
 ADDR = 'localhost'
 PORT = 6666
+senha=1234
+userDict={"alan":"1234","teste":"0000","batata":"frita"}
+
 COMMANDS=["connect user,password","pwd","chdir path","getfiles","getdirs","exit"]
 
 
@@ -11,36 +14,58 @@ serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind((ADDR, PORT))
 serversocket.listen(5)
 
-def handle_response(received):
+def handle_login(received):
     response=" "
+    logged=0
 
-    if received =="exit":
-        clientsocket.close()
-    elif received =="connect":
-        response = "conected"
+    #while logged==0  and received != "exit" :
+    
+    if received.split()[0]!="connect":
+        response="Conecte o usuario primeiro.'connect user,senha' "
     else:
-        response= " ".join(COMMANDS)
+        user =  received.split()[1].split(',')[0]
+        senha = received.split()[1].split(',')[1]
+        #Faz autenticacao
+        if user in userDict :
+            if senha==userDict[user]:
+                response=f"Usuario {user} logado com sucesso"
+                logged=1 
+            else: 
+                response="senha incorreta" 
 
-    return response
+        else :
+            response=f"Usuario {user} não encontrado"
+            
+    # Uma vez autenticado, realiza comandos
+    return response,logged
 
 
 def threaded_client(clientsocket):
-    while True:
+    logged=0
+    received=' '
+
+    while received!="exit":
+    
         try:
             received = clientsocket.recv(2048).decode()
-            if not received:
-                continue
-
-            enviado = handle_response(received)
-            clientsocket.sendall(str.encode(enviado))
-
-            print(f"Recebido : {received}")
-
         except socket.error as e:
             print(e)
             print("CONEXÃO FECHADA")
             clientsocket.close()
-            break
+            
+        print(f"Recebido : {received}")
+
+        if not logged:
+            response,logged = handle_login(received)
+            clientsocket.sendall(str.encode(response))
+        else :
+            #Já está logado, agora realizamos os comandos
+            response= "Logou!!!"
+            if received not in COMMANDS:
+               response= "All commands are :"+("\n\t".join(COMMANDS))
+            clientsocket.sendall(str.encode(response))
+
+
 
 # ------ Main Server Loop ------
 while True:
