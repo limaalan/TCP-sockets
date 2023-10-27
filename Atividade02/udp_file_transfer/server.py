5d65848138e1ba8249e18ee1fabd22d26af9d9a7
limaalan <alanlimamarques6@gmail.com>
2023-10-27 09:51:03 -0300

    removendo comentarios

--- Atividade02/udp_file_transfer/server.py
+++ Atividade02/udp_file_transfer/server.py
@@ -9,7 +9,7 @@
         
 PROTOCOLO : 
     Request :
-    [FILENAME] [CHECKSUM]           [NUMBER_BLOCKS]  
+    [FILENAME]; [CHECKSUM]    ;      [NUMBER_BLOCKS]  
     (255 bytes) (40 byte, SHA1)     (2 byte) -> Capacidade máxima de arquivo = 64MB 
 
     Response:
@@ -43,12 +43,6 @@ def thread_envia(client_socket):
         checksum=request[1]
         quantidade_datagramas=int(request[2])
 
-        #Diminui tamanho arquivo
-        #Copia-se o arquivo em blocos de 1024 bytes
-        # for i in range (quantidade_datagramas):
-        #     data,_ = client_socket.recvfrom(1024)
-        #     arquivo+=data
-
         arquivo = open('server_directory/'+nome_arquivo,'wb')
         for i in range(quantidade_datagramas):
             print(f"Recebendo bloco {i+1}/{quantidade_datagramas}")
@@ -69,12 +63,6 @@ def thread_envia(client_socket):
             #Fazendo um hash do arquivo e comparando com o recebido
             arquivo = open('server_directory/'+nome_arquivo,'rb')
             sha1_hash = hashlib.sha1()
-           
-           #Diminui tamanho arquivo
-            # while True:
-            #     checksum_server=arquivo.read(1024)
-            #     if not checksum_server:break
-            #     sha1_hash.update(checksum_server)
 
             with open ("server_directory/"+nome_arquivo,'rb') as file:
                 checksum_server=file.read()
@@ -102,7 +90,6 @@ def main():
     #despacha as threads
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     client_socket.bind(endereco)
-    #start_new_thread(thread_envia, (client_socket,))
     thread_envia(client_socket)
 
 
